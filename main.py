6f11e50cc9944016bf8d009b72ced58fbf7a48e5
Fabian Peña <fabiancpl91@gmail.com>
2023-11-02 13:34:06 -0500

    Updating data model

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-   return { "message": "Hello world" }
+    return { "message": "Hello world" }
 
 @app.post("/predict")
 def make_predictions(X: List[DataModel]):
